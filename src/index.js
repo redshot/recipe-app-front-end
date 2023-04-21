cd42dc751ffbd1f0d4c2971053ca07849d8dcfa6
Adrian Berenguel <adrianberenguel90@gmail.com>
2023-04-21 17:57:28 +0800

    Removed unnecessary for this project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,3 +14,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/**
+ * Our entire react app or you can say the App component is rendered inside a <div="root"></div> with a root id
+ * 
+ */
